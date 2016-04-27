b3a2e883a3992323aa8018b1bdadd2a90b68c3cb
Steven Turner <sturner1123@gmail.com>
2016-04-26 22:48:59 -0400

    added gitignore and added time tracking to functions

--- tailfib.cpp
+++ tailfib.cpp
@@ -18,15 +18,14 @@ ld tailfact(int n, ld a){
 	if(n == 0 || n == 1){
 		return a;
 	}
-
-	a *= n;
-	return tailfact(n-1, a);
+	return tailfact(n-1, a*n);
 }
 
 
 int main(){
 	int n;
 	ld tb, tf;
+	double tb_time, tf_time;
 	while (1) {
 		std::cout.precision(5);
 		std::cout << "Enter a non neg number pls: ";
@@ -34,18 +33,26 @@ int main(){
 		if(n >= 0){
 			std::cout << std::scientific;
 			//std::cout << std::fixed;
+			int start = clock();
 			tb = tailfib(n,0,1);
+			int end  = clock();
+			tb_time = (end - start)/double(CLOCKS_PER_SEC)*1000;
+			start = clock();
 			tf = tailfact(n,1);
+			end  = clock();
+			tf_time = (end - start)/double(CLOCKS_PER_SEC)*1000;
 			if(!isinf(tb)){
 				std::cout << "Fib result: " << tb << std::endl;
 			} else {
 				std::cout << "Fib result too large for a long double" << std::endl;
 			}
+			std::cout << std::fixed <<"Calculated in " << tb_time << " seconds" << std::scientific << std::endl;
 			if(!isinf(tf)){
 				std::cout << "Fact result: " << tf << std::endl;
 			} else {
 				std::cout << "Factorial result too large for a long double" << std::endl;
 			}
+			std::cout << std::fixed <<"Calculated in " << tf_time << " seconds" << std::scientific << std::endl;
 		} else {
 			break;
 		}
